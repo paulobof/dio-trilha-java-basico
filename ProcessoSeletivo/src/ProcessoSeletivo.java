10de7bce57f1c95e3c9089d52972517bd0a7626e
paulobof <paulobof@gmail.com>
2023-09-20 18:48:08 -0300

    Analisando candidatos

--- ProcessoSeletivo/src/ProcessoSeletivo.java
+++ ProcessoSeletivo/src/ProcessoSeletivo.java
@@ -2,6 +2,22 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class ProcessoSeletivo {
     public static void main(String[] args) {
+        System.out.println("Processo seletivo");
+        analisarCandidato(1900.00);
+        analisarCandidato(2000.00);
+        analisarCandidato(2100.00);
 
     }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase>salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase==salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+
 }
